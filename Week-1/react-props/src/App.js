47dff072ea30d5d706f62f7d9b35c921d9bfe458
Gary J <github@accu.ca>
2021-12-08 20:30:13 -0800

    Commit

---
+++ Week-1/react-props/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Image from 'components/Image';
+import Header from 'components/Header';
+import Description from 'components//Description';
+
+export default function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      <Image />
+      <Description />
+    </div>
+  );
+}
